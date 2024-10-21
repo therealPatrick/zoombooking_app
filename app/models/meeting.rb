4caebeb3fb35f63756da309b54ac7ea7795a12ca
therealPatrick <malamashangala13@gmail.com>
2024-10-21 15:40:07 +0200

    creating zoom meeting with API implemented

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,7 @@
 class Meeting < ApplicationRecord
+	include Zoom
+
+
 	has_many :bookings
 
 	validates :topic, presence: true, length: {maximum: 50}
@@ -6,4 +9,37 @@ class Meeting < ApplicationRecord
 	validates :start_time, presence: true
 	validates :duration, presence: true
 
+	after_create :create_zoom_meeting
+	after_update :update_zoom_meeting
+
+
+	def create_zoom_meeting
+		payload = {
+			topic: self.topic,
+			password: self.password,
+			start_time: self.start_time.to_datetime.strftime("%Y-%m-%dT%H:%M:%S"),
+			duration: self.duration,
+			time_zone: "Zambia/Lusaka"
+		}
+		
+		# create new meeting on zoom server
+		zoom_meeting = Zoom::MeetingService.new.create_meeting(payload)
+		data = JSON.parse(zoom_meeting)
+		puts data
+
+		# save zoom_id to database 
+		self.zoom_id = data["id"].to_i
+		self.save
+	end
+
+	def update_zoom_meeting
+		payload = {
+			topic: self.topic,
+			password: self.password,
+			start_time: self.start_time.to_datetime.strftime("%Y-%m-%dT%H:%M:%S"),
+			duration: self.duration,
+		}
+		# update the existing meeting 
+		Zoom::MeetingService.new.update_meeting(self.zoom_id,payload)
+	end
 end
